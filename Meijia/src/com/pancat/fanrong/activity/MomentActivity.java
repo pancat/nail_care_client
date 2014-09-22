719c897efb922cda1a2a3c1f1092de30d193b4b3
realxie <jinyun2007@126.com>
2014-09-22 20:31:15 +0800

    圈子下拉事件

--- Meijia/src/com/pancat/fanrong/activity/MomentActivity.java
+++ Meijia/src/com/pancat/fanrong/activity/MomentActivity.java
@@ -9,11 +9,14 @@ import com.pancat.fanrong.R.layout;
 import android.app.ListActivity;
 import android.app.Activity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 import android.widget.AbsListView;
 import android.widget.AbsListView.OnScrollListener;
 import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 public class MomentActivity extends ListActivity implements OnScrollListener{
 
@@ -25,6 +28,8 @@ public class MomentActivity extends ListActivity implements OnScrollListener{
 		List<String> items = getItemList();
 		ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,android.R.layout.simple_list_item_1, items);
 		setListAdapter(adapter);
+		
+		super.getListView().setOnScrollListener(this);
 	}
 	
 	protected List<String> getItemList()
@@ -35,17 +40,24 @@ public class MomentActivity extends ListActivity implements OnScrollListener{
 		return list;
 	}
 	
+	@Override
+	protected void onListItemClick(ListView l, View v, int position, long id)
+	{
+		Log.e("log","" + position + ", " + id);
+	}
 
 	@Override
 	public void onScroll(AbsListView view, int firstVisibleItem,
 			int visibleItemCount, int totalItemCount) {
 		// TODO Auto-generated method stub
 		
+		Log.e("scroll", "" + firstVisibleItem + ", " + visibleItemCount + ", " + totalItemCount);
 	}
 
 	@Override
 	public void onScrollStateChanged(AbsListView view, int scrollState) {
 		// TODO Auto-generated method stub
 		
+		Log.e("scroll", "" + scrollState);
 	}
 }
