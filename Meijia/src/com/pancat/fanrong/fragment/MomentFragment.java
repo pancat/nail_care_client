742ea6ac40ba37ffbee0c001254e8dad92dbca70
realxie <jinyun2007@126.com>
2014-09-23 23:00:04 +0800

    圈子fragment

--- Meijia/src/com/pancat/fanrong/fragment/MomentFragment.java
+++ Meijia/src/com/pancat/fanrong/fragment/MomentFragment.java
@@ -1,21 +1,80 @@
 package com.pancat.fanrong.fragment;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import com.pancat.fanrong.R;
 
 import android.annotation.SuppressLint;
 import android.app.Fragment;
+import android.app.ListFragment;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AbsListView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import android.os.Bundle;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.AbsListView;
+import android.widget.AbsListView.OnScrollListener;
+import android.widget.Adapter;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 @SuppressLint("NewApi")
-public class MomentFragment extends Fragment{
+public class MomentFragment extends ListFragment{
 
 	@Override
 	public View onCreateView(LayoutInflater inflater, ViewGroup container,
 			Bundle savedInstanceState) {
 		return inflater.inflate(R.layout.fragment_moment, container, false);
+	}
+	
+	
+	@Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        
+    	List<String> items = getItemList();
+		ArrayAdapter<String> adapter = new ArrayAdapter<String>(getActivity(),android.R.layout.simple_list_item_1, items);
+		setListAdapter(adapter);
+		
+		//getListView().setOnScrollListener(this);
+        
+    }
+	
+	protected List<String> getItemList()
+	{
+		List<String> list = new ArrayList<String>();
+		for (int i = 1; i < 100; i ++)
+			list.add("abc");
+		return list;
+	}
+	
+	@Override
+	public void onListItemClick(ListView l, View v, int position, long id)
+	{
+		Log.e("log","" + position + ", " + id);
+	}
+
+	public void onScroll(AbsListView view, int firstVisibleItem,
+			int visibleItemCount, int totalItemCount) {
+		// TODO Auto-generated method stub
+		
+		Log.e("scroll", "" + firstVisibleItem + ", " + visibleItemCount + ", " + totalItemCount);
+	}
+
+	public void onScrollStateChanged(AbsListView view, int scrollState) {
+		// TODO Auto-generated method stub
+		
+		Log.e("scroll", "" + scrollState);
 	}
 	
 }
