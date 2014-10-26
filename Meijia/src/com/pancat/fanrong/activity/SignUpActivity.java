a5e66bf8bbe0486182571ef4fd7da3077e51678e
epmark <epmark@126.com>
2014-10-26 11:45:50 +0800

    个人注册

--- Meijia/src/com/pancat/fanrong/activity/SignUpActivity.java
+++ Meijia/src/com/pancat/fanrong/activity/SignUpActivity.java
@@ -171,22 +171,22 @@ public class SignUpActivity extends Activity {
 		// return email.contains("@");
 		boolean b=false;
 		if(email.contains("@")){
-			//ÅÐ¶ÏemailÊÇ·ñºÏ·¨
+			//ï¿½Ð¶ï¿½emailï¿½Ç·ï¿½Ï·ï¿½
 
 			Pattern p=Pattern.compile("\\w+@(\\w+.)+[a-z]{2,3}");
 		    Matcher m=p.matcher(email);
 		    b=m.matches();
-		    if(b)Log.i("emial","emailºÏ·¨");
+		    if(b)Log.i("emial","emailï¿½Ï·ï¿½");
 		    else
 		    	showerror(getString(R.string.error_format));
-		    	Log.i("emial","email²»ºÏ·¨");
+		    	Log.i("emial","emailï¿½ï¿½ï¿½Ï·ï¿½");
 		}else if(email.length()==11){
-			Log.i("emial","ÊÖ»ú³¤¶ÈºÏ·¨£¬¿ªÊ¼ÑéÖ¤");
-			//ÑéÖ¤ÊÖ»úºÅºÏ·¨
+			Log.i("emial","ï¿½Ö»ï¿½ï¿½ï¿½ï¿½ÈºÏ·ï¿½ï¿½ï¿½ï¿½ï¿½Ê¼ï¿½ï¿½Ö¤");
+			//ï¿½ï¿½Ö¤ï¿½Ö»ï¿½ï¿½ÅºÏ·ï¿½
 			Pattern p=Pattern.compile("[0-9]+");
 		    Matcher m=p.matcher(email);
 		    b=m.matches();
-		    if(b)Log.i("emial","ÊÖ»úºÏ·¨");
+		    if(b)Log.i("emial","ï¿½Ö»ï¿½ï¿½Ï·ï¿½");
 		    else
 		    	showerror(getString(R.string.error_format));
 		}else{
@@ -204,15 +204,15 @@ public class SignUpActivity extends Activity {
 		// TODO: Replace this with your own logic
 		boolean b=false;
 
-		Pattern p=Pattern.compile("[a-zA-Z0-9]{1£¬8}");
+		Pattern p=Pattern.compile("[a-zA-Z0-9]{1ï¿½ï¿½8}");
 	    Matcher m=p.matcher(password);
 	    b=m.matches();
 		if(b){
-			Log.i("emial","ÃÜÂëºÏ·¨");
+			Log.i("emial","ï¿½ï¿½ï¿½ï¿½Ï·ï¿½");
 			return true;
 		}
 		else{
-			Log.i("emial","ÃÜÂë²»ºÏ·¨");
+			Log.i("emial","ï¿½ï¿½ï¿½ë²»ï¿½Ï·ï¿½");
 		return false;
 		}
 	}
@@ -301,18 +301,18 @@ public class SignUpActivity extends Activity {
 
              HttpClient httpClient = new DefaultHttpClient();
 
-             //ÕâÀïÊÇÄãÓë·þÎñÆ÷½»»¥µÄµØÖ·
+             //ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½Äµï¿½Ö·
              String validateUrl = "http://ec2-54-169-66-69.ap-southeast-1.compute.amazonaws.com/nail_care_test/index.php/user/register";
 
          //    System.out.println(validateUrl);
-             //ÉèÖÃÁ´½Ó³¬Ê±
+             //ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½Ó³ï¿½Ê±
              httpClient.getParams().setParameter(CoreConnectionPNames.
                                  CONNECTION_TIMEOUT, 5000);
-             //ÉèÖÃ¶ÁÈ¡³¬Ê±
+             //ï¿½ï¿½ï¿½Ã¶ï¿½È¡ï¿½ï¿½Ê±
              httpClient.getParams().setParameter(
                                  CoreConnectionPNames.SO_TIMEOUT, 5000);
 
-             //×¼±¸´«ÊäµÄÊý¾Ý
+             //×¼ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½
              List<NameValuePair> paramsq = new ArrayList<NameValuePair>();
              System.out.println("mEmail="+mEmail);
              System.out.println("mPassword="+mPassword);
@@ -323,17 +323,17 @@ public class SignUpActivity extends Activity {
              HttpPost httpRequest = new HttpPost(validateUrl);
 
              try {
-            	//·¢ËÍÇëÇó
+            	//ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½
 				httpRequest.setEntity(new UrlEncodedFormEntity(paramsq, HTTP.UTF_8));
 
-	            //µÃµ½ÏìÓ¦
+	            //ï¿½Ãµï¿½ï¿½ï¿½Ó¦
 	            HttpResponse response = httpClient.execute(httpRequest);
 	            if(response.getStatusLine().getStatusCode() == 200)
                 {
-	            	Log.i("http", "µÃµ½ÁËÏìÓ¦");
+	            	Log.i("http", "ï¿½Ãµï¿½ï¿½ï¿½ï¿½ï¿½Ó¦");
 	            	 StringBuilder builder = new StringBuilder();
 
-                     //½«µÃµ½µÄÊý¾Ý½øÐÐ½âÎö
+                     //ï¿½ï¿½ï¿½Ãµï¿½ï¿½ï¿½ï¿½ï¿½ï¿½Ý½ï¿½ï¿½Ð½ï¿½ï¿½ï¿½
                      BufferedReader buffer = new BufferedReader(
                                          new InputStreamReader(response.getEntity().getContent()));
 
@@ -341,11 +341,11 @@ public class SignUpActivity extends Activity {
                      {   builder.append(s);  }
 
                      System.out.println("hahahahah"+builder.toString());
-                     //µÃµ½Json¶ÔÏó
+                     //ï¿½Ãµï¿½Jsonï¿½ï¿½ï¿½ï¿½
                      Log.i("http", "json=: "+builder.toString());
                      JSONObject jsonObject   = new JSONObject(builder.toString());
 
-                     //Í¨¹ýµÃµ½¼üÖµ¶ÔµÄ·½Ê½µÃµ½Öµ
+                     //Í¨ï¿½ï¿½ï¿½Ãµï¿½ï¿½ï¿½Öµï¿½ÔµÄ·ï¿½Ê½ï¿½Ãµï¿½Öµ
                  	code = jsonObject.getInt("code");
         			Log.i("http", "code=: "+code);
 
@@ -371,8 +371,8 @@ public class SignUpActivity extends Activity {
 			showProgress(false);
 
 			if (code==1) {
-				// ×¢²á³É¹¦
-				Toast.makeText(SignUpActivity.this, "×¢²á³É¹¦", Toast.LENGTH_LONG).show();
+				// ×¢ï¿½ï¿½É¹ï¿½
+				Toast.makeText(SignUpActivity.this, "×¢ï¿½ï¿½É¹ï¿½", Toast.LENGTH_LONG).show();
 
 				Bundle bundle = new Bundle();
 				bundle.putString("mAccount", mEmail);
@@ -385,17 +385,17 @@ public class SignUpActivity extends Activity {
 
 				switch(code){
 				case 101:
-					errstring="ÓÃ»§ÃûÃÜÂë×éºÏ²»ºÏ·¨";
-					//Toast.makeText(SignUpActivity.this, "ÓÃ»§ÃûÃÜÂë×éºÏ²»ºÏ·¨", Toast.LENGTH_LONG).show();
-					Log.i("signup","code 101 ÓÃ»§ÃûÃÜÂë×éºÏ²»ºÏ·¨");break;
+					errstring="ï¿½Ã»ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½Ï²ï¿½ï¿½Ï·ï¿½";
+					//Toast.makeText(SignUpActivity.this, "ï¿½Ã»ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½Ï²ï¿½ï¿½Ï·ï¿½", Toast.LENGTH_LONG).show();
+					Log.i("signup","code 101 ï¿½Ã»ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½Ï²ï¿½ï¿½Ï·ï¿½");break;
 				case 102:
-					errstring="ÓÃ»§ÃûÒÑ¾­±»×¢²á";
-					//Toast.makeText(SignUpActivity.this, "ÓÃ»§ÃûÒÑ¾­±»×¢²á", Toast.LENGTH_LONG).show();
-					Log.i("signup","code 102 ÓÃ»§ÃûÒÑ¾­±»×¢²á");break;
+					errstring="ï¿½Ã»ï¿½ï¿½ï¿½ï¿½Ñ¾ï¿½ï¿½ï¿½×¢ï¿½ï¿½";
+					//Toast.makeText(SignUpActivity.this, "ï¿½Ã»ï¿½ï¿½ï¿½ï¿½Ñ¾ï¿½ï¿½ï¿½×¢ï¿½ï¿½", Toast.LENGTH_LONG).show();
+					Log.i("signup","code 102 ï¿½Ã»ï¿½ï¿½ï¿½ï¿½Ñ¾ï¿½ï¿½ï¿½×¢ï¿½ï¿½");break;
 				case 103:
-					errstring="·þÎñÆ÷»òÊý¾Ý¿â¹ÊÕÏ";
-					//Toast.makeText(SignUpActivity.this, "·þÎñÆ÷»òÕßÊý¾Ý¿â¹ÊÕÏ", Toast.LENGTH_LONG).show();
-					Log.i("signup","code 103 ·þÎñÆ÷»òÕßÊý¾Ý¿â¹ÊÕÏ");	break;
+					errstring="ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½Ý¿ï¿½ï¿½ï¿½ï¿½";
+					//Toast.makeText(SignUpActivity.this, "ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½Ý¿ï¿½ï¿½ï¿½ï¿½", Toast.LENGTH_LONG).show();
+					Log.i("signup","code 103 ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½Ý¿ï¿½ï¿½ï¿½ï¿½");	break;
 				default:
 					break;
 				}
@@ -432,246 +432,3 @@ public class SignUpActivity extends Activity {
 		mPasswordView.requestFocus();
 	}
 }
-=======
-package com.pancat.fanrong.activity;
-
-import android.animation.Animator;
-import android.animation.AnimatorListenerAdapter;
-import android.annotation.TargetApi;
-import android.app.Activity;
-import android.os.AsyncTask;
-import android.os.Build;
-import android.os.Bundle;
-import android.text.TextUtils;
-import android.view.KeyEvent;
-import android.view.View;
-import android.view.View.OnClickListener;
-import android.view.inputmethod.EditorInfo;
-import android.widget.Button;
-import android.widget.EditText;
-import android.widget.TextView;
-
-import com.pancat.fanrong.R;
-
-/**
- * A login screen that offers login via email/password.
- */
-public class SignUpActivity extends Activity {
-
-	/**
-	 * A dummy authentication store containing known user names and passwords.
-	 * TODO: remove after connecting to a real authentication system.
-	 */
-	private static final String[] DUMMY_CREDENTIALS = new String[] {
-			"foo@example.com:hello", "bar@example.com:world" };
-	/**
-	 * Keep track of the login task to ensure we can cancel it if requested.
-	 */
-	private UserLoginTask mAuthTask = null;
-
-	// UI references.
-	private EditText mAccount;
-	private EditText mPasswordView;
-	private View mProgressView;
-	private View mLoginFormView;
-
-	@Override
-	protected void onCreate(Bundle savedInstanceState) {
-		super.onCreate(savedInstanceState);
-		setContentView(R.layout.activity_sign_up);
-
-		// Set up the login form.
-		mAccount = (EditText) findViewById(R.id.account);
-
-		mPasswordView = (EditText) findViewById(R.id.password);
-		mPasswordView
-				.setOnEditorActionListener(new TextView.OnEditorActionListener() {
-					@Override
-					public boolean onEditorAction(TextView textView, int id,
-							KeyEvent keyEvent) {
-						if (id == R.id.login || id == EditorInfo.IME_NULL) {
-							attemptLogin();
-							return true;
-						}
-						return false;
-					}
-				});
-
-		Button mEmailSignInButton = (Button) findViewById(R.id.email_sign_in_button);
-		mEmailSignInButton.setOnClickListener(new OnClickListener() {
-			@Override
-			public void onClick(View view) {
-				attemptLogin();
-			}
-		});
-
-		mLoginFormView = findViewById(R.id.login_form);
-		mProgressView = findViewById(R.id.login_progress);
-	}
-
-
-
-	/**
-	 * Attempts to sign in or register the account specified by the login form.
-	 * If there are form errors (invalid email, missing fields, etc.), the
-	 * errors are presented and no actual login attempt is made.
-	 */
-	public void attemptLogin() {
-		if (mAuthTask != null) {
-			return;
-		}
-
-		// Reset errors.
-		mAccount.setError(null);
-		mPasswordView.setError(null);
-
-		// Store values at the time of the login attempt.
-		String email = mAccount.getText().toString();
-		String password = mPasswordView.getText().toString();
-
-		boolean cancel = false;
-		View focusView = null;
-
-		// Check for a valid password, if the user entered one.
-		if (!TextUtils.isEmpty(password) && !isPasswordValid(password)) {
-			mPasswordView.setError(getString(R.string.error_invalid_password));
-			focusView = mPasswordView;
-			cancel = true;
-		}
-
-		// Check for a valid email address.
-		if (TextUtils.isEmpty(email)) {
-			mAccount.setError(getString(R.string.error_field_required));
-			focusView = mAccount;
-			cancel = true;
-		} else if (!isEmailValid(email)) {
-			mAccount.setError(getString(R.string.error_invalid_email));
-			focusView = mAccount;
-			cancel = true;
-		}
-
-		if (cancel) {
-			// There was an error; don't attempt login and focus the first
-			// form field with an error.
-			focusView.requestFocus();
-		} else {
-			// Show a progress spinner, and kick off a background task to
-			// perform the user login attempt.
-			showProgress(true);
-			mAuthTask = new UserLoginTask(email, password);
-			mAuthTask.execute((Void) null);
-		}
-	}
-
-	private boolean isEmailValid(String email) {
-		// TODO: Replace this with your own logic
-		//return email.contains("@");
-		return true;
-	}
-
-	private boolean isPasswordValid(String password) {
-		// TODO: Replace this with your own logic
-		return password.length() >= 3;
-	}
-
-	/**
-	 * Shows the progress UI and hides the login form.
-	 */
-	@TargetApi(Build.VERSION_CODES.HONEYCOMB_MR2)
-	public void showProgress(final boolean show) {
-		// On Honeycomb MR2 we have the ViewPropertyAnimator APIs, which allow
-		// for very easy animations. If available, use these APIs to fade-in
-		// the progress spinner.
-		if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.HONEYCOMB_MR2) {
-			int shortAnimTime = getResources().getInteger(
-					android.R.integer.config_shortAnimTime);
-
-			mLoginFormView.setVisibility(show ? View.GONE : View.VISIBLE);
-			mLoginFormView.animate().setDuration(shortAnimTime)
-					.alpha(show ? 0 : 1)
-					.setListener(new AnimatorListenerAdapter() {
-						@Override
-						public void onAnimationEnd(Animator animation) {
-							mLoginFormView.setVisibility(show ? View.GONE
-									: View.VISIBLE);
-						}
-					});
-
-			mProgressView.setVisibility(show ? View.VISIBLE : View.GONE);
-			mProgressView.animate().setDuration(shortAnimTime)
-					.alpha(show ? 1 : 0)
-					.setListener(new AnimatorListenerAdapter() {
-						@Override
-						public void onAnimationEnd(Animator animation) {
-							mProgressView.setVisibility(show ? View.VISIBLE
-									: View.GONE);
-						}
-					});
-		} else {
-			// The ViewPropertyAnimator APIs are not available, so simply show
-			// and hide the relevant UI components.
-			mProgressView.setVisibility(show ? View.VISIBLE : View.GONE);
-			mLoginFormView.setVisibility(show ? View.GONE : View.VISIBLE);
-		}
-	}
-
-
-	/**
-	 * Represents an asynchronous login/registration task used to authenticate
-	 * the user.
-	 */
-	public class UserLoginTask extends AsyncTask<Void, Void, Boolean> {
-
-		private final String mEmail;
-		private final String mPassword;
-
-		UserLoginTask(String email, String password) {
-			mEmail = email;
-			mPassword = password;
-		}
-
-		@Override
-		protected Boolean doInBackground(Void... params) {
-			// TODO: attempt authentication against a network service.
-
-			try {
-				// Simulate network access.
-				Thread.sleep(2000);
-			} catch (InterruptedException e) {
-				return false;
-			}
-
-			for (String credential : DUMMY_CREDENTIALS) {
-				String[] pieces = credential.split(":");
-				if (pieces[0].equals(mEmail)) {
-					// Account exists, return true if the password matches.
-					return pieces[1].equals(mPassword);
-				}
-			}
-
-			// TODO: register the new account here.
-			return true;
-		}
-
-		@Override
-		protected void onPostExecute(final Boolean success) {
-			mAuthTask = null;
-			showProgress(false);
-
-			if (success) {
-				finish();
-			} else {
-				mPasswordView
-						.setError(getString(R.string.error_incorrect_password));
-				mPasswordView.requestFocus();
-			}
-		}
-
-		@Override
-		protected void onCancelled() {
-			mAuthTask = null;
-			showProgress(false);
-		}
-	}
-}
-
